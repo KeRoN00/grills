4981073222869b28ef8800003360ca06b85b0a21
KeRoN00 <kero1475@gmail.com>
2022-11-20 19:33:29 +0100

    Added more styling into gallery (carousel)

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React  from "react";
 import classes from "./Gallery.module.css";
 import GalleryItem from "./GalleryItem";
 
 const Gallery = () => {
+ 
+  //console.log(liczba);
   return (
     <div className={classes.gallery} id="Gallery">
-      <div className={classes.carousel}>
-        <GalleryItem />
+      <div className={classes.carousel} data-simplebar
+      
+      >
         <GalleryItem />
         <GalleryItem />
         <GalleryItem />
--- src/Components/GalleryItem.js
+++ src/Components/GalleryItem.js
@@ -2,7 +2,15 @@ import React from 'react'
 import classes from "./GalleryItem.module.css"
 const GalleryItem = () => {
   return (
-    <div className={classes.card}>GalleryItem</div>
+    <div className={classes.card}>
+      <div className={classes.image}>
+         zdjecie
+      </div>
+      <div className={classes.description}>
+      info
+
+      </div>
+    </div>
   )
 }
 
