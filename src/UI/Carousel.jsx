baa2dfa9cf3555b7092bc68c99d46186566801ec
KeRoN00 <listwon.dev@gmail.com>
2023-03-21 14:30:35 +0100

    Draggable Gallery Feature

--- src/UI/Carousel.jsx
+++ src/UI/Carousel.jsx
@@ -1,14 +1,38 @@
 
-import React from 'react'
+import React, {useState, useRef} from 'react'
 import classes from "./Carousel.module.css"
 import Card from "../UI/Card";
 const Carousel = ({title, content}) => {
-    
+  const [isDragging, setIsDragging] = useState(false);
+  const [startX, setStartX] = useState(null);
+  const [scrollLeft, setScrollLeft] = useState(0);
+  const galleryRef = useRef(null);
+
+  const handleMouseDown = (event) => {
+    setIsDragging(true);
+    setStartX(event.pageX - galleryRef.current.offsetLeft);
+    setScrollLeft(galleryRef.current.scrollLeft);
+  }
+
+  const handleMouseMove = (event) => {
+    event.preventDefault();
+    if (!isDragging) return;
+    const x = event.pageX - galleryRef.current.offsetLeft;
+    const walk = (x - startX) * 2;
+    galleryRef.current.scrollLeft = scrollLeft - walk;
+  }
+
+  const handleMouseUp = () => {
+    setIsDragging(false);
+  }
 
     return (
     <div className={classes.galleryView}>
         <h1>{title}</h1>
-        <div className={classes.carousel}>
+        <div className={classes.carousel}  ref={galleryRef} onMouseDown={handleMouseDown}
+      onMouseMove={handleMouseMove}
+      onMouseUp={handleMouseUp}
+      onMouseLeave={handleMouseUp}>
         {content.map(zestaw => (
             <Card key={zestaw.id} imagePath={zestaw.imgPath}/>
         ))}
