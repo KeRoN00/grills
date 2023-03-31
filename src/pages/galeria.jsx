b3740c38f8dfd6e3520d761cc796dfceb2cf97a9
KeRoN00 <listwon.dev@gmail.com>
2023-03-31 14:30:56 +0200

    Fixed weird navbar behaviour

--- src/pages/galeria.jsx
+++ src/pages/galeria.jsx
@@ -1,15 +1,23 @@
 import { Gallery, Navbar } from '@/Components'
+import Head from 'next/head'
 import React from 'react'
 
-const Galeria = () => {
+export default function Galeria() {
   return (
-    <div>
+    <>
+      <Head>
+        <title>Kominki - GL</title>
+        <meta name="description" content="Kominki z wędzarnią i wielofunkcyjne zestawy" />
+        <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, target-densityDpi=device-dpi"
+    />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className='xd'>
       <Navbar />
       <Gallery />
-    </div>
+      </main>
+    </>
   )
 }
-
-
-
-export default Galeria
